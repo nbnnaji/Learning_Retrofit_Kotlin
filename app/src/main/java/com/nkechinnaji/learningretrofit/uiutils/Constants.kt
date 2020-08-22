10a4b2a9df280a45938ff6d35a8693f6d812ecc2
nbnnaji <nbnnaji@yahoo.co.uk>
2020-08-21 21:51:30 -0500

    Added service generator class

---
+++ app/src/main/java/com/nkechinnaji/learningretrofit/uiutils/Constants.kt
@@ -0,0 +1,10 @@
+package com.nkechinnaji.learningretrofit.uiutils
+
+
+/**
+ * Created by Nkechi Nnaji on 8/21/20.
+ * Description:
+ */
+object Constants {
+    const val BASE_URL: String = "http://api.github.com/"
+}
--- app/src/main/java/com/nkechinnaji/learningretrofit/view/MainActivity.kt
+++ app/src/main/java/com/nkechinnaji/learningretrofit/view/MainActivity.kt
@@ -6,16 +6,15 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.nkechinnaji.learningretrofit.R
 import com.nkechinnaji.learningretrofit.model.GitHubRepo
+import com.nkechinnaji.learningretrofit.model.ServiceGenerator
 import com.nkechinnaji.learningretrofit.service.GitHubClient
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     var adapter: GitHubRepoAdapter? = null
 
@@ -24,32 +23,27 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 
 
-        val builder: Retrofit.Builder = Retrofit.Builder()
-            .baseUrl("https://api.github.com")
-            .addConverterFactory(GsonConverterFactory.create())
-
-        val retrofit: Retrofit = builder.build()
-
-        val client: GitHubClient = retrofit.create(GitHubClient::class.java)
-        val call= client.reposForUser("nbnnaji")
+        val client: GitHubClient = ServiceGenerator.createService(GitHubClient::class.java)
+        val call = client.reposForUser("nbnnaji")
 
 
-        call.enqueue(object: Callback<List<GitHubRepo>> {
+        call.enqueue(object : Callback<List<GitHubRepo>> {
             override fun onFailure(call: Call<List<GitHubRepo>>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "error :(", Toast.LENGTH_LONG).show()
             }
 
-            override fun onResponse(call: Call<List<GitHubRepo>>, response: Response<List<GitHubRepo>>) {
+            override fun onResponse(
+                call: Call<List<GitHubRepo>>,
+                response: Response<List<GitHubRepo>>
+            ) {
 
-                if(response.isSuccessful){
+                if (response.isSuccessful) {
                     val repos: List<GitHubRepo>? = response.body()
                     adapter = repos?.let { GitHubRepoAdapter(this@MainActivity, it) }
                     pagination_list.adapter = adapter
-                }
-                else{
+                } else {
                     Log.e("RETROFIT TEST: ", "Not sure you are working!");
                 }
-
 
 
             }
